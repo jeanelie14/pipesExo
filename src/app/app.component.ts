dcde4fbfacba4ca8b4db64113f29f5bb413d4f21
jeanelie14 <jeune14@gmail.com>
2023-04-01 19:24:36 -0400

    slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'LesPipes';
-  nombre:string = 'Captain america'
+  title = 'LesPipes'
+  nombre: string = 'Jean Elie'
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
