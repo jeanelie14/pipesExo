81693895c6277600d254c04f9b50856d057cce99
jeanelie14 <jeune14@gmail.com>
2023-04-01 21:41:41 -0400

    pipe decimal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
   title = 'LesPipes'
   nombre: string = 'Jean Elie'
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  personaje: string[] = ['Iroman', 'Spiderman', 'Thor', 'Loki', 'Groot']
+  PI: number = Math.PI
 }
